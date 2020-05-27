a5f6513229c0d70ace08cd6f40eaa2015c848ce6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-27 11:04:25 -0400

    Add test it exists

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,5 +3,9 @@ require 'minitest/pride'
 require './lib/exhibit'
 
 class ExhibitTest < MiniTest::Test
-  
+  def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+  end
 end
