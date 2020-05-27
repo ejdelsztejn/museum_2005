2bc81a89ddb308fcdf858cf50e8bf1a5ce7331a3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-27 11:08:51 -0400

    Add patron test it has attributes

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -4,8 +4,16 @@ require './lib/patron'
 
 class PatronTest < MiniTest::Test
   def test_it_exists
-    patron = Patron.new("Bob", 20)
+    patron_1 = Patron.new("Bob", 20)
 
-    assert_instance_of Patron, patron
+    assert_instance_of Patron, patron_1
+  end
+
+  def test_it_has_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
   end
 end
